ced4adfc41cf26cb3763fdaae4364fc87858c7bc
Tobias Quante <t.quante@outlook.com>
2021-09-05 20:15:27 +0200

    add travis file

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  base: '/vitepress-components',
   title: 'Radunia components',
   description: 'This Vitepress documentation serves static Vue.js components, sample code and snippets. Each of these may or may not be used in the Radunia project.',
   lang: 'en-US',
