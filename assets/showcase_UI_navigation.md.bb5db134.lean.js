458528142c37a9bc81b3734bea15cf7e9fc90020
Tobias Quante <t.quante@outlook.com>
2021-09-20 20:32:25 +0200

    deploy

---
+++ assets/showcase_UI_navigation.md.bb5db134.lean.js
@@ -0,0 +1,1 @@
+import{o as t,c as a}from"./app.bc8bcc5c.js";const e='{"title":"","description":"","frontmatter":{},"relativePath":"showcase/UI/navigation.md","lastUpdated":1629913415069}',r={};r.render=function(e,r,o,c,i,n){return t(),a("div")};export{e as __pageData,r as default};
